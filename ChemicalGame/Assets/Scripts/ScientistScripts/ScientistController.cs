6ff8de247700c21ac5bf71445fe5ca3a51339f92
Viggo <vhjort20@student.aau.dk>
2023-04-11 13:41:07 +0200

    Added UI asset

---
+++ ChemicalGame/Assets/Scripts/ScientistScripts/ScientistController.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ScientistController : MonoBehaviour
+{
+
+    public NavMeshAgent scientist;
+    public Animator scientistAnimator;
+    public GameObject scientistTargetDest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
