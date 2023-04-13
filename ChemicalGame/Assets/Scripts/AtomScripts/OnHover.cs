d5689534500423f45b652268e6ca7fef70e3d95d
Nicolaj Frederiksen <72729883+NicolajFrederiksen@users.noreply.github.com>
2023-04-13 12:23:28 +0200

    tryyy

--- ChemicalGame/Assets/Scripts/AtomScripts/OnHover.cs
+++ ChemicalGame/Assets/Scripts/AtomScripts/OnHover.cs
@@ -7,6 +7,7 @@ public class OnHover : MonoBehaviour
     public Outline outline;
     public Camera cam;
     public bool isOver, isPicked, picking;
+    
 
     void Start()
     {
@@ -36,6 +37,8 @@ public class OnHover : MonoBehaviour
             outline.enabled = true;
         }
         isOver = true;
+        
+        
     }
 
     void OnMouseExit()
