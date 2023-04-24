288b020dcb3973ac469b1d09e8f3f25b908742fd
Viggo <vhjort20@student.aau.dk>
2023-04-24 13:28:54 +0200

    Added zoomFunction to camera

---
+++ ChemicalGame/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] private float zoomSpeed = 2f;
+    [SerializeField] private float zoomFOV = 1f;
+    [SerializeField] private float originalFOV = 60f;
+
+    private bool isZooming = false;
+    private void Start()
+    {
+        GetComponent<Camera>().fieldOfView = originalFOV;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            isZooming = true;
+            GetComponent<Camera>().fieldOfView = zoomFOV;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            isZooming = false;
+            GetComponent<Camera>().fieldOfView = originalFOV;
+        }
+
+        if (isZooming)
+        {
+            GetComponent<Camera>().fieldOfView -= zoomSpeed * Time.deltaTime;
+            GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView, zoomFOV, originalFOV);
+        }
+    }
+}
